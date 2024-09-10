8ed70e9404eae8ab05d32efe7590add05e1bb4c9
nitikornbunya <nitikorn.bunya@gmail.com>
2024-09-11 01:45:35 +0700

    mobile vie

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import "./AppHeader.css";
 function AppHeader() {
   return (
     <header className="app-header">
-      <img className="app-header-logo" src="/images/pple-logo.png"></img>
+      <img className="app-header-logo" alt="pple-logo" src="/images/pple-logo.png"></img>
     </header>
   );
 }
