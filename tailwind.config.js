527dab27d7e24375890513a1a230cd35862814dc
nitikornbunya <nitikorn.bunya@gmail.com>
2024-09-28 02:09:39 +0700

    fix: tailwind path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/*.js",
-    "./src/**/*.js",
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.ts',
+    './src/**/*.tsx',
   ],
   theme: {
     extend: {
