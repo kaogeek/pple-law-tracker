38be1016ac1c734d15764bcf7cffa0e0080bb3c0
nitikornbunya <nitikorn.bunya@gmail.com>
2024-09-29 20:32:15 +0700

    edit: api url, remove github workflow

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@
 export const fetchLawsData = async () => {
     try {
       const response = await fetch(
-        "https://raw.githubusercontent.com/kaogeek/pple-law-tracker/refs/heads/main/public/law.json"
+        "https://raw.githubusercontent.com/kaogeek/pple-law-tracker-api/refs/heads/main/data/law.json"
       );
       if (!response.ok) {
         throw new Error("Failed to fetch data");
